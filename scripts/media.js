764ff45629ffe9cfcf9d6f5c2ecbd2b2952e3a21
balas <balas@DESKTOP-FFNIKMF>
2020-07-04 23:00:02 +0300

    optimization for mobile devices

---
+++ scripts/media.js
@@ -0,0 +1,7 @@
+if ((window.matchMedia('(max-width: 521px')).matches) {
+    // set placeholder as "Enter the name"
+
+    nameForm.name.placeholder = "Введите имя";
+    newChatForm.message.placeholder = "Введите сообщение"
+    // set placeholder as "Enter the message"
+}
